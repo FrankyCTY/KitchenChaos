79fa722be3f9eb95e8db2e44fe6ed6c2cc822e85
Franky Chan <franky@datch.io>
2023-05-06 18:56:00 +1200

    Make use of player animation & refactor input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
     private void Update()
     {
-        Debug.Log(Input.GetKey(KeyCode.W) ? "Pressing!" : "-");
+        var inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        
+        // Moving direction
+        transform.position += moveDirection * (Time.deltaTime * this.moveSpeed);
+        // Face direction
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * this.rotateSpeed);
+        
+        this.isWalking = moveDirection != Vector3.zero;
+    }
+
+    public bool IsWalking()
+    {
+        return this.isWalking;
     }
 }
