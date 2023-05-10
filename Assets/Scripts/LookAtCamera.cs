3ef1f2a2d29af3add3d5977fedb1216b5a5dc68b
Franky Chan <franky@datch.io>
2023-05-10 23:37:42 +1200

    Add look at camera inverted

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -21,6 +21,14 @@ public class LookAtCamera : MonoBehaviour
                 transform.LookAt(Camera.main.transform);
                 break;
             case Mode.LookAtInverted:
+                /// Imagine this object's position (transform.position) = 0, 0, 0
+                /// Camera's position (Camera.main.position) = 1,0,1
+                /// transform.position - Camera.main.transform.position = -1, 0, -1 -> Which is the direction where the camera
+                /// looking at that will see through this object.
+                ///
+                /// Look inverted:
+                /// Instead of from object seeing to the camera
+                /// We basically look at the inverted direction :)
                 Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
                 transform.LookAt(transform.position + directionFromCamera );
                 break;
